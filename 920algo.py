d4b0d5065c741cbf447d83aa49ba1e30a0e559ae
GK <117473489+GKM15@users.noreply.github.com>
2023-04-29 23:50:20 +0530

    datetimeeverywhere

--- 920algo.py
+++ 920algo.py
@@ -20,7 +20,7 @@ import json
 
 
 dhan= dhanhq(client_id=dhancred.client_id(),access_token=dhancred.access_token())
-print(f"Session to Broker Dhan Established at {datetime.datetime.now()}Hope You saved your Credentils")
+print(f"{datetime.datetime.now()} Session to Broker Dhan Established ,Hope You saved your Credentils")
 
 
 
@@ -34,17 +34,17 @@ underlying = "NIFTY"
 nearestexpiry = expiry_list(underlying)
 expiry= nearestexpiry[0]
 
-print(f'Retrieving List of Expires')
+print(f'{datetime.datetime.now()} Retrieving List of Expires')
 
-print(f'List of Expires available {nearestexpiry[0:3]}')
+print(f'{datetime.datetime.now()} List of Expires available {nearestexpiry[0:3]}')
 
-print(f'Selected Expiry is {expiry}')
+print(f'{datetime.datetime.now()} Selected Expiry is {expiry}')
 
 ##############################################################################################
 
 # Downloading Scrip Master of Dhan
 
-print("Trying to Download Scrip Master from Broker ~15 MB File....")
+print(f"{datetime.datetime.now()} Trying to Download Scrip Master from Broker ~15 MB File....")
 security_idsource = "https://images.dhan.co/api-data/api-scrip-master.csv"
 
 todays_date= datetime.datetime.now().strftime("%Y-%m-%d")
@@ -54,7 +54,7 @@ file_name= f'api-scrip-master.csv'
 response = requests.get(security_idsource)
 with open(file_name, "wb") as f:
     f.write(response.content)
-    print(f"Scrip Master '{file_name}' downloaded successfully.")
+    print(f"{datetime.datetime.now()} Scrip Master '{file_name}' downloaded successfully.")
 
 #Created a Polars Data Frame for Placing Orders
 required_cols = ['SEM_EXM_EXCH_ID', 'SEM_SMST_SECURITY_ID', 'SEM_INSTRUMENT_NAME', 'SEM_TRADING_SYMBOL', 'SEM_CUSTOM_SYMBOL', 'SEM_EXPIRY_DATE', 'SEM_EXPIRY_FLAG']
@@ -65,18 +65,18 @@ master = pl.read_csv("api-scrip-master.csv", columns=required_cols)
 while True:
     current_time = datetime.datetime.now().time()
     if current_time >= datetime.time(9, 20, 00):  #9,20,30
-        print(f"It is now {datetime.datetime.now()}. Continuing...")
+        print(f"{datetime.datetime.now()} It is now {datetime.datetime.now()}. Continuing...")
         break
     else:
-        print(f"It is currently {current_time}. Waiting for Time Specified")
+        print(f"{datetime.datetime.now()} It is currently {current_time}. Waiting for Time Specified")
         time.sleep(1) # sleep for 1 second before checking Time Specified
 
 ###############################################################################
 
 ltp= (nse_quote_ltp(underlying))
-print(f'LTP of {underlying} is {ltp}')
+print(f'{datetime.datetime.now()} LTP of {underlying} is {ltp}')
 ATM = round(ltp/50)*50
-print(f'At The Money Strike Of Underlying {underlying} is {ATM}')
+print(f'{datetime.datetime.now()} At The Money Strike Of Underlying {underlying} is {ATM}')
 expiry_date_string = expiry
 expiry_date = datetime.datetime.strptime(expiry_date_string, '%d-%b-%Y')
 year = expiry_date.year
@@ -87,13 +87,13 @@ atmCE= f'NIFTY {day} {month} {ATM} CALL'
 
 atmPE= f'NIFTY {day} {month} {ATM} PUT'
 
-print(f"At The Money Call Strike is {atmCE}, At The Money Put Strike is {atmPE}")
+print(f"{datetime.datetime.now()} At The Money Call Strike is {atmCE}, At The Money Put Strike is {atmPE}")
 
 LTPofCE= float(nse_quote_ltp(underlying,"latest","CE",ATM))
-print(f'Call is Now Trading at {LTPofCE}')
+print(f'{datetime.datetime.now()} Call is Now Trading at {LTPofCE}')
 
 LTPofPE= float(nse_quote_ltp(underlying,"latest","PE",ATM))
-print(f'Put is Now Trading at {LTPofPE}')
+print(f'{datetime.datetime.now()} Put is Now Trading at {LTPofPE}')
 
 # Saving SL as LTP of CE and PE Minus SL as defined earlier Before placing order
 
@@ -115,10 +115,10 @@ peSL= float(round(LTPofPE+SL,1)) #SL in Points
 securityidCE = master.filter(pl.col("SEM_CUSTOM_SYMBOL") == atmCE)["SEM_SMST_SECURITY_ID"][0]
 securityidPE = master.filter(pl.col("SEM_CUSTOM_SYMBOL") == atmPE)["SEM_SMST_SECURITY_ID"][0]
 
-print(f'Retrieved Security Id of Boker for CE is {securityidCE}')
-print(f'Retrieved Security Id of Boker for PE is {securityidPE}')
+print(f'{datetime.datetime.now()} Retrieved Security Id of Boker for CE is {securityidCE}')
+print(f'{datetime.datetime.now()} Retrieved Security Id of Boker for PE is {securityidPE}')
 
-print(f'placing Market Orders for {atmCE} and {atmPE}, Time is {datetime.datetime.now()}')
+print(f'{datetime.datetime.now()} placing Market Orders for {atmCE} and {atmPE}, Time is {datetime.datetime.now()}')
 
 ########################################################################################
 
@@ -129,7 +129,7 @@ dhan.place_order(security_id=str(securityidCE),
     order_type=dhan.MARKET,
     product_type=dhan.INTRA,
     price=0)
-print(f'Market Order Placed for {atmCE}, Time is {datetime.datetime.now()}')
+print(f'{datetime.datetime.now()} Market Order Placed for {atmCE}, Time is {datetime.datetime.now()}')
 
 dhan.place_order(security_id=str(securityidPE),  
     exchange_segment=dhan.FNO,
@@ -138,17 +138,17 @@ dhan.place_order(security_id=str(securityidPE),
     order_type=dhan.MARKET,
     product_type=dhan.INTRA,
     price=0)
-print(f'Market Order Placed for {atmPE}, Time is {datetime.datetime.now()}')
+print(f'{datetime.datetime.now()} Market Order Placed for {atmPE}, Time is {datetime.datetime.now()}')
 
 ################################################################################
-print(f"CEentryprice is {ce_entry_price}")
-print(f"PEentryprice is {pe_entry_price}")
-print(f"CESL is now {ceSL}")
-print(f"PESL is now {peSL}")
+print(f"{datetime.datetime.now()} CEentryprice is {ce_entry_price}")
+print(f"{datetime.datetime.now()} PEentryprice is {pe_entry_price}")
+print(f"{datetime.datetime.now()} CESL is now {ceSL}")
+print(f"{datetime.datetime.now()} PESL is now {peSL}")
 
 ###############################################################################
 
-print("All Positions are Punched, Wating for SL")
+print(f"{datetime.datetime.now()} All Positions are Punched, Wating for SL")
 
 traded = "No"
 while traded == "No":
@@ -156,24 +156,24 @@ while traded == "No":
     try:
         LTPofCE=nse_quote_ltp(underlying,"latest","CE",ATM)
         LTPofPE=nse_quote_ltp(underlying,"latest","PE",ATM)
-        print("Check Started for SL and Exit")
+        print(f"{datetime.datetime.now()} Check Started for SL and Exit")
         if ((LTPofCE > ceSL) or (dt.hour >= 15 and dt.minute >= 15)):
             exitCE= dhan.place_order(security_id=str(securityidCE),  exchange_segment=dhan.FNO,transaction_type=dhan.BUY,quantity=Qty,order_type=dhan.MARKET,product_type=dhan.INTRA,price=0)
             traded = "CE"
-            print(f"Exiting CE, LTP of CE is {LTPofCE}")
+            print(f"{datetime.datetime.now()} Exiting CE, LTP of CE is {LTPofCE}")
             time.sleep(3)
         elif((LTPofPE > peSL) or (dt.hour >= 15 and dt.minute >= 15)):
 
             exitPE= dhan.place_order(security_id=str(securityidPE),  exchange_segment=dhan.FNO,transaction_type=dhan.BUY,quantity=Qty,order_type=dhan.MARKET,product_type=dhan.INTRA,price=0)
             traded = "PE"
-            print(f"Exiting PE, LTP of PE is {LTPofPE}")
+            print(f"{datetime.datetime.now()} Exiting PE, LTP of PE is {LTPofPE}")
             time.sleep(3)
         else:
-            print(f"No SL is Hit, LTP of CE is {LTPofCE} and LTP of PE is {LTPofPE}")
+            print(f"{datetime.datetime.now()} No SL is Hit, LTP of CE is {LTPofCE} and LTP of PE is {LTPofPE}")
             time.sleep(3)
             
     except:
-        print("Couldn't find LTP , RETRYING !!")
+        print(f"{datetime.datetime.now()} Couldn't find LTP , RETRYING !!")
         time.sleep(3)
         
     if(traded=="CE"):
@@ -185,14 +185,14 @@ while traded == "No":
                 if ((ltp > peSL) or (dt.hour >= 15 and dt.minute >= 15)):
                     exitPE= dhan.place_order(security_id=str(securityidPE),  exchange_segment=dhan.FNO,transaction_type=dhan.BUY,quantity=Qty,order_type=dhan.MARKET,product_type=dhan.INTRA,price=0)
                     traded = "Close"
-                    print(f"Exiting PE, LTP of PE is {LTPofPE}")
+                    print(f"{datetime.datetime.now()} Exiting PE, LTP of PE is {LTPofPE}")
                     time.sleep(3)
                 else:
-                    print(f"PE SL not hit,LTP of PE is {LTPofPE}")
+                    print(f"{datetime.datetime.now()} PE SL not hit,LTP of PE is {LTPofPE}")
                     time.sleep(1)
                   
             except:
-                print("Couldn't find LTP , RETRYING !!")
+                print(f"{datetime.datetime.now()} Couldn't find LTP , RETRYING !!")
                 time.sleep(1)
 
     elif(traded == "PE"):
@@ -207,11 +207,11 @@ while traded == "No":
                     time.sleep(3)
                     
                 else:
-                    print(f"CE SL not hit,LTP of CE is {LTPofCE}")
+                    print(f"{datetime.datetime.now()} CE SL not hit,LTP of CE is {LTPofCE}")
                     time.sleep(3)
             except:
-                print("Couldn't find LTP , RETRYING !!")
+                print(f"{datetime.datetime.now()} Couldn't find LTP , RETRYING !!")
                 time.sleep(1)
     elif (traded == "Close"):
-        print("All trades done. Exiting Code")
+        print(f"{datetime.datetime.now()} All trades done. Exiting Code")
         
